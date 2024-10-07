c531c707c25ea5b6ca053a5b8d616ade21aa2f7b
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-10-07 20:56:58 +0500

    setup all routes for the pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import AuthLayout from "./components/auth/Layout";
+import Login from "./pages/auth/login";
+import Register from "./pages/auth/register";
+import AdminLayout from "./components/admin/Layout";
+import AdminDashboard from "./pages/admin/Dasboard";
+import AdminProducts from "./pages/admin/Products";
+import AdminOrders from "./pages/admin/Orders";
+import AdminFeatures from "./pages/admin/Features";
+import ShoppingLayout from "./components/shopping/Layout";
+import NotFound from "./pages/not-found";
+import ShoppingHome from "./pages/shopping/Home";
+import ShoppingListing from "./pages/shopping/Listing";
+import ShoppingCheckout from "./pages/shopping/Checkout";
+import ShoppingAccount from "./pages/shopping/Account";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col overflow-hidden bg-white">
+      {/* {common component} */}
+      <h1>header component</h1>
+      <Routes>
+        {/* auth routes */}
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+
+        {/* admin routes */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="products" element={<AdminProducts />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="features" element={<AdminFeatures />} />
+        </Route>
+
+        {/* shopping routes */}
+        <Route path="/shop" element={<ShoppingLayout />}>
+          <Route path="home" element={<ShoppingHome />} />
+          <Route path="listing" element={<ShoppingListing />} />
+          <Route path="checkout" element={<ShoppingCheckout />} />
+          <Route path="account" element={<ShoppingAccount />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
