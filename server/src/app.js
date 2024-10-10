891c1acdffba7d569dbd963a0d3ab6d1bef93c3d
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-10-10 22:38:41 +0500

    add user route

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { userRoutes } from "./routes/user.route";
 
 const app = express();
 
@@ -22,5 +23,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+
+app.use("/api/v1/users", userRoutes);
 
 export { app };
