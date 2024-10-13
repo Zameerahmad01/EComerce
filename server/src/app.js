ef0bb1167cb14ca5f28ed2b703cdad0db71a9abe
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-10-13 23:20:25 +0500

    fix bug

--- server/src/app.js
+++ server/src/app.js
@@ -6,13 +6,13 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: [
       "Content-Type",
       "Authorization",
       "Cache-control",
-      "Exprires",
+      "Expires",
       "pragma",
     ],
     credentials: true,
