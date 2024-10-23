3dc63d13ae72020e642c31418d8789e3a8b9bfd6
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-10-23 21:50:12 +0500

    add route for check authentication

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -5,11 +5,16 @@ import {
   logoutUser,
 } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/check-auth").get(verifyJWT, (req, res) => {
+  const user = req.user;
+  res.status(200).json(new ApiResponse(200, user, "Authenticated user"));
+});
 
 export default router;
