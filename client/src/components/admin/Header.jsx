32daf1d98bf92790013a605271c508941385c61d
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-10-24 22:05:10 +0500

    admin header

--- client/src/components/admin/Header.jsx
+++ client/src/components/admin/Header.jsx
@@ -1,5 +1,21 @@
-const AdminHeader = () => {
-  return <div>Admin header</div>;
+import { Button } from "../ui/button";
+import { AlignJustify, LogOut } from "lucide-react";
+
+const AdminHeader = ({ open, setOpen }) => {
+  return (
+    <header className="flex justify-center items-center px-4 py-3 bg-background border-b">
+      <Button onClick={() => setOpen(!open)} className="lg:hidden sm:block ">
+        <AlignJustify />
+        <span className="sr-only">Toggle Menu</span>
+      </Button>
+      <div className="flex flex-1 justify-end">
+        <Button className="inline-flex items-center gap-2 rounded-md px-4 py-2 text-sm font-medium shadow">
+          <LogOut />
+          Logout
+        </Button>
+      </div>
+    </header>
+  );
 };
 
 export default AdminHeader;
