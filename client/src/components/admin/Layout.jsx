5bc0172e35023615dc2df41060c58fea4ba08dd4
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-10-24 22:07:06 +0500

    add header and sidebar

--- client/src/components/admin/Layout.jsx
+++ client/src/components/admin/Layout.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom";
 import AdminSidebar from "./Sidebar";
 import AdminHeader from "./Header";
+import { useState } from "react";
 
 const AdminLayout = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="flex min-h-screen w-full">
       {/* amin side work */}
-      <AdminSidebar />
+      <AdminSidebar open={open} setOpen={setOpen} />
       <div className="flex flex-1 flex-col">
         {/* admin header */}
-        <AdminHeader />
+        <AdminHeader open={open} setOpen={setOpen} />
         <main className="flex flex-1 bg-muted/40 p-4 md:p-6">
           <Outlet />
         </main>
