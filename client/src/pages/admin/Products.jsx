70d9450b461817999612676aefe1254083246c01
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-10-26 19:53:27 +0500

    add new product page for admin

--- client/src/pages/admin/Products.jsx
+++ client/src/pages/admin/Products.jsx
@@ -1,5 +1,57 @@
+import CommonForm from "@/components/common/form";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetHeader } from "@/components/ui/sheet";
+import { addProductFormElements } from "@/config";
+import { useState } from "react";
+import ProductImageUpload from "@/components/admin/Image-upload";
+
+const initialFormData = {
+  image: "",
+  title: "",
+  description: "",
+  category: "",
+  brand: "",
+  price: "",
+  salePrice: "",
+  totalStock: "",
+};
 const AdminProducts = () => {
-  return <div> AdminProducts </div>;
+  const [openCreateProduct, setOpenCreateProduct] = useState(false);
+  const [formData, setFormData] = useState(initialFormData);
+  const [imageFile, setImageFile] = useState(null);
+  const [uploadImageUrl, setUploadImageUrl] = useState("");
+
+  function onSubmit() {}
+  return (
+    <>
+      <div className="mb-5 w-full flex justify-end">
+        <Button onClick={() => setOpenCreateProduct(true)}>
+          Add New Product
+        </Button>
+      </div>
+      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
+      <Sheet open={openCreateProduct} onOpenChange={setOpenCreateProduct}>
+        <SheetContent side="right" className="overflow-auto">
+          <SheetHeader>Add New Product</SheetHeader>
+          <ProductImageUpload
+            imageFile={imageFile}
+            setImageFile={setImageFile}
+            uploadImageUrl={uploadImageUrl}
+            setUploadImageUrl={setUploadImageUrl}
+          />
+          <div className="py-6">
+            <CommonForm
+              formData={formData}
+              setFormData={setFormData}
+              buttonText={"Add"}
+              onSubmit={onSubmit}
+              formControls={addProductFormElements}
+            />
+          </div>
+        </SheetContent>
+      </Sheet>
+    </>
+  );
 };
 
 export default AdminProducts;
