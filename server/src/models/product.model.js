4ebe70b4fb3a396a39b43e7ad9f0c86bb5e8bf73
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-10-27 23:20:27 +0500

    product model

---
+++ server/src/models/product.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const productSchema = new Schema(
+  {
+    image: String,
+    title: String,
+    description: String,
+    category: String,
+    brand: String,
+    price: Number,
+    salePrice: Number,
+    totalStock: Number,
+  },
+  { timestamps: true }
+);
+
+export const Product = mongoose.model("Product", productSchema);
