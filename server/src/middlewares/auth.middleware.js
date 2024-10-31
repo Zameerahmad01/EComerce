357061e27eecc4695a121a07e1d4576252c05ada
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-10-31 20:50:06 +0500

    fix bug

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
