61d49514affcffbda2595520792d0259f223a098
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-11-03 23:01:07 +0500

    shopping product tile

---
+++ client/src/components/shopping/Product-tile.jsx
@@ -0,0 +1,58 @@
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardContent,
+} from "@/components/ui/card";
+import { Badge } from "../ui/badge";
+import { Button } from "../ui/button";
+function ShoppingProductTile({ product }) {
+  return (
+    <Card className="w-full max-w-sm mx-auto">
+      <div>
+        <div className="relative">
+          <img
+            src={product?.image}
+            alt={product?.title}
+            className="w-full h-[250px] object-cover rounded-t-lg"
+          />
+          {product?.salePrice > 0 ? (
+            <Badge className="absolute top-2 left-2 bg-red-500 hover:bg-red-600">
+              Sale
+            </Badge>
+          ) : null}
+        </div>
+        <CardContent className="p-4">
+          <h2 className="text-xl font-bold mb-2">{product?.title}</h2>
+          <div className="flex items-center justify-between mb-2">
+            <span className="text-sm text-muted-foreground capitalize">
+              {product?.category}
+            </span>
+            <span className="text-sm text-muted-foreground capitalize">
+              {product?.brand}
+            </span>
+          </div>
+          <div className="flex items-center justify-between">
+            <span
+              className={`${
+                product?.salePrice > 0 ? "line-through" : ""
+              } text-lg font-semibold text-primary`}
+            >
+              {product?.price}
+            </span>
+            {product?.salePrice > 0 ? (
+              <span className="text-lg font-semibold text-primary">
+                {product?.salePrice}
+              </span>
+            ) : null}
+          </div>
+        </CardContent>
+        <CardFooter className="p-2">
+          <Button className="w-full">Add to Cart</Button>
+        </CardFooter>
+      </div>
+    </Card>
+  );
+}
+
+export default ShoppingProductTile;
