4a4af8ff2ca31066661580f7c403bff1646df218
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-11-03 23:00:04 +0500

    shopping product slice

---
+++ client/src/store/shop/product-slice/index.js
@@ -0,0 +1,38 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+const initialState = {
+  productsList: [],
+  isLoading: false,
+};
+
+export const getFilteredProducts = createAsyncThunk(
+  "products/getFilteredProducts",
+  async () => {
+    const response = await axios.get(
+      "http://localhost:8000/api/v1/shop/products/get"
+    );
+    return response?.data;
+  }
+);
+
+const shopProductSlice = createSlice({
+  name: "shoppingProducts",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getFilteredProducts.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(getFilteredProducts.fulfilled, (state, action) => {
+      // console.log(action.payload, "action.payload");
+      state.isLoading = false;
+      state.productsList = action.payload.data;
+    });
+    builder.addCase(getFilteredProducts.rejected, (state) => {
+      state.isLoading = false;
+      state.productsList = [];
+    });
+  },
+});
+
+export default shopProductSlice.reducer;
