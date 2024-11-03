95bdf828999229c86cbc00a963190e6d4d3ce9a5
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-11-03 23:00:16 +0500

    add shopping product reducer

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
 import adminProductSlice from "./admin/index";
+import shopProductSlice from "./shop/product-slice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     adminProducts: adminProductSlice,
+    shopProducts: shopProductSlice,
   },
 });
 
